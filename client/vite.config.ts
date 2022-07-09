23f1f2546d2b81abf7be4891030115a5f4b5610d
meretamal <meretamal@uc.cl>
2022-07-09 12:15:22 -0400

    chore(docker): dockerize client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  }
 })

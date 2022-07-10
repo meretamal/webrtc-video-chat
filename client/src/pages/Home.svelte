a94461add38469c6fb9ac95493d5c925bc7af2e7
meretamal <meretamal@uc.cl>
2022-07-09 20:38:03 -0400

    feat(session): manage user input when submiting username

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -1,10 +1,19 @@
+<script>
+  import { username } from "../stores/session";
+</script>
+
 <div
   class="container h-full is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
 >
   <h1 class="has-text-centered is-size-1">Choose your username</h1>
   <form class="w-80 my-5">
     <div class="field">
-      <input type="text" class="input" placeholder="e.g Alex" />
+      <input
+        type="text"
+        class="input"
+        placeholder="e.g Alex"
+        bind:value={$username}
+      />
     </div>
     <div class="field is-flex is-justify-content-center">
       <input

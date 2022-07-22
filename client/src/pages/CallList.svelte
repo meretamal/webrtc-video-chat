dcf45a3cc75da7cda5126906bac47d541bf114ff
meretamal <meretamal@uc.cl>
2022-07-22 16:30:26 -0400

    feat: translate text to english

--- client/src/pages/CallList.svelte
+++ client/src/pages/CallList.svelte
@@ -13,7 +13,7 @@
 <div class="container my-5">
   <h3 class="is-size-4 has-text-grey">{$username}</h3>
   <div class="is-flex is-justify-content-space-between is-align-items-flex-end">
-    <h2 class="is-size-1">Llamadas</h2>
-    <button class="button is-success"> Crear llamada </button>
+    <h2 class="is-size-1">Calls</h2>
+    <button class="button is-success">Start a new call</button>
   </div>
 </div>

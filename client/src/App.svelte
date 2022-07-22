bb25e144b03328cb5ed31598e4ed01a8b3f1f438
meretamal <meretamal@uc.cl>
2022-07-22 16:21:12 -0400

    feat: add a router

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import Home from "./pages/Home.svelte";
+  import Router from "./Router.svelte";
 </script>
 
 <main>
-  <Home />
+  <Router />
 </main>
---
+++ client/src/Router.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-navigator";
+  import Home from "./pages/Home.svelte";
+</script>
+
+<Router>
+  <Route path="/" component={Home} />
+</Router>

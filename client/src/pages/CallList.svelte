c6aa72851d53e2fe884b57d64e1a4c6dcebac2f6
meretamal <meretamal@uc.cl>
2022-09-11 18:30:03 -0300

    feat(calls): create call

--- client/src/pages/CallList.svelte
+++ client/src/pages/CallList.svelte
@@ -1,12 +1,24 @@
-<script>
-  import { onMount } from "svelte";
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
   import { navigate } from "svelte-navigator";
   import { username } from "../stores/session";
+  import { setCallInformation } from "../stores/call";
+  import { socket } from "../services/socket";
+  import type { Call } from "../interfaces/call";
 
   onMount(() => {
     if (!$username) {
       navigate("/", { replace: true });
     }
+
+    socket.on("call-joined", (call: Call) => {
+      setCallInformation(call);
+      navigate(`/calls/${call.id}`);
+    });
+  });
+
+  onDestroy(() => {
+    socket.removeAllListeners();
   });
 </script>
 
@@ -14,6 +26,11 @@
   <h3 class="is-size-4 has-text-grey">{$username}</h3>
   <div class="is-flex is-justify-content-space-between is-align-items-flex-end">
     <h2 class="is-size-1">Calls</h2>
-    <button class="button is-success">Start a new call</button>
+    <button
+      class="button is-success"
+      on:click={() => socket.emit("create-call", { username: $username })}
+    >
+      Start a new call
+    </button>
   </div>
 </div>
---
+++ client/src/stores/call.ts
@@ -0,0 +1,12 @@
+import { writable } from 'svelte/store';
+import type { Call } from 'src/interfaces/call';
+
+export const id = writable<string>(null);
+export const caller = writable<string>(null);
+export const callee = writable<string | null>(null);
+
+export const setCallInformation = (data: Call) => {
+  id.set(data.id);
+  caller.set(data.caller);
+  callee.set(data.callee);
+};

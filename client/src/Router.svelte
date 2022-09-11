16c28d71578c5c484db6ceedc81f1b52f69287c0
meretamal <meretamal@uc.cl>
2022-09-11 18:29:43 -0300

    feat(calls): add a call page component

--- client/src/Router.svelte
+++ client/src/Router.svelte
@@ -2,9 +2,11 @@
   import { Router, Route } from "svelte-navigator";
   import Home from "./pages/Home.svelte";
   import CallList from "./pages/CallList.svelte";
+  import Call from "./pages/Call.svelte";
 </script>
 
 <Router>
   <Route path="/" component={Home} />
   <Route path="calls" component={CallList} />
+  <Route path="calls/:id" component={Call} />
 </Router>
---
+++ client/src/pages/Call.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<div>
+  <h1>Call</h1>
+</div>
+
+<style>
+</style>

24f268e97aa0f10c27a8fdb53901b4af4f4e172f
meretamal <meretamal@uc.cl>
2022-09-14 08:33:57 -0300

    feat(calls): display indicators to show the call status

--- client/src/pages/Call.svelte
+++ client/src/pages/Call.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
+  import { SyncLoader } from "svelte-loading-spinners";
+  import { onMount } from "svelte";
+  import { socket } from "../services/socket";
+  import { callee, setCallInformation } from "../stores/call";
+
+  let ready = false;
+
+  onMount(() => {
+    socket.on("call-joined", (call) => {
+      setCallInformation(call);
+    });
+  });
 </script>
 
-<div>
-  <h1>Call</h1>
+<div
+  class="container is-full is-flex is-flex-direction-column is-justify-content-center is-align-items-center"
+>
+  {#if !ready}
+    {#if !$callee}
+      <p class="is-size-2">Waiting for someone to join</p>
+    {:else}
+      <p class="is-size-2">Waiting for the call to begin</p>
+    {/if}
+    <SyncLoader color="#23d160" />
+  {/if}
 </div>
 
 <style>
+  .is-full {
+    width: 100vw;
+    min-height: 100vh;
+  }
 </style>
